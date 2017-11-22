22dc94a222c3837189e9c83af9c4d5c6d757ad17
Grzegorz Janiszewski <zamowienia@biurotfc.pl>
2017-11-22 10:52:51 +0100

    basic configuration sequelize

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+//set up express app
+const app = express();
+
+//log requests to the console
+app.use(logger("dev"));
+
+//parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//setup a default catch-all route that sends back a welcome message in JSON
+
+app.get("*", (req, res) => res.status(200).send({
+    message: "Witam w kursie"
+}));
+
+module.exports = app;
